433aa172ebdac9c1601908920cc3515f332f87f7
dengdihao <dengdihao_888@163.com>
2019-01-16 19:34:16 +0800

    vue-lic3.0

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,38 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <!--左侧导航-->
+    <left-nav></left-nav>
+    <!--操作区域-->
+    <div class="main"><router-view /></div>
   </div>
 </template>
 
-<style lang="less">
+<script>
+import leftNav from "./components/commom/leftNav.vue";
+
+export default {
+  name: "app",
+  components: {
+    leftNav
+  }
+};
+</script>
+
+<style>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
+  height: 100%;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+
+.main {
+  float: left;
+  width: 95%;
+  background-color: #eff2f7;
+  height: 100%;
+  overflow: auto;
 }
 </style>
--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 
+import pos from "./components/pos.vue";
+
 Vue.use(Router);
 
 export default new Router({
@@ -10,8 +12,8 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home
+      name: "pos",
+      component: pos
     },
     {
       path: "/about",
